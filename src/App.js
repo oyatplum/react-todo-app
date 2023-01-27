2ae5f73cbdef2910c62a60ddd19577f2498dc7e9
yeji <oyatpeach@naver.com>
2023-01-27 14:12:49 +0900

    add todo list

--- src/App.js
+++ src/App.js
@@ -42,6 +42,22 @@ export default class App extends Component {
     this.setState({todoData : newTodoData})
   }
 
+  handleChange = (e) => {
+    this.setState({value: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
+      completed: false
+    }
+
+    this.setState({ todoData: [...this.state.todoData, newTodo]})
+  }
+
   render() {
     return(
       <div className="container">
@@ -58,6 +74,23 @@ export default class App extends Component {
             <button style={this.btnStyle} onClick = {() => this.handleClick(data.id)}>x</button>
           </div>
         ))}
+
+        <form style = {{display: "flex"}} onSubmit = {this.handleSubmit}>
+          <input 
+            type = "text"
+            name = "value"
+            style = {{flex: "10", padding: "5px"}}
+            placeholder = "해야 할 일을 입력하세요."
+            value = {this.state.value}
+            onChange = {this.handleChange}
+          />
+          <input 
+            type = "submit"
+            value = "입력"
+            className= "btn"
+            style= {{flex: "1"}}
+          />
+        </form>
         </div>
       </div>
     )
