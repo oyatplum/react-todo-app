7611f5cd6a3a29ec18fd1758d72456d98448b051
yeji <oyatpeach@naver.com>
2023-01-27 15:10:53 +0900

    update check box

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import "./App.css";
 export default class App extends Component {
 
   state = {
-    todoData : [
-      {
-        id: "1",
-        title: "공부하기",
-        completed: true
-      },
-      {
-        id: "2",
-        title: "청소하기",
-        completed: false
-      },
-    ],
+    todoData : [],
     value: "",
   }
 
@@ -28,11 +17,11 @@ export default class App extends Component {
     float: "right"
   }
 
-  getStyle = () => {
+  getStyle = (completed) => {
     return {
       padding: "10px",
       borderBottom: "1px #ccc dotted",
-      textDecoration: "none"
+      textDecoration: completed ? "line-through" : "none"
     }
   }
 
@@ -58,6 +47,17 @@ export default class App extends Component {
     this.setState({ todoData: [...this.state.todoData, newTodo]})
   }
 
+  handleCompleteChange = (id) => {
+    let newTodoData = this.state.todoData.map(data => {
+      if(data.id === id) {
+        data.completed = !data.completed
+      }
+      return data
+    })
+
+    this.setState({todoData: newTodoData})
+  }
+
   render() {
     return(
       <div className="container">
@@ -68,8 +68,8 @@ export default class App extends Component {
 
 
         {this.state.todoData.map((data) => (
-          <div style={this.getStyle()} key = {data.id}>
-            <input type="checkbox" defaultChecked={false} />
+          <div style={this.getStyle(data.completed)} key = {data.id}>
+            <input type="checkbox" defaultChecked={false} onChange = {() => this.handleCompleteChange(data.id)} />
               {data.title}
             <button style={this.btnStyle} onClick = {() => this.handleClick(data.id)}>x</button>
           </div>
