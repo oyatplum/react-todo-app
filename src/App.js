b8bc7365f8355e2757fe21d873dd06288ecdfcb0
yeji <oyatpeach@naver.com>
2023-01-27 12:53:36 +0900

    delete todo list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import React, {Component} from "react";
 import "./App.css";
 
 export default class App extends Component {
+
+  state = {
+    todoData : [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false
+      },
+    ],
+    value: "",
+  }
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -19,18 +36,11 @@ export default class App extends Component {
     }
   }
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true
-    },
-    {
-      id: "2",
-      title: "청소하기",
-      completed: false
-    }
-  ]
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter(data => data.id !== id)
+    console.log('newTodoData', newTodoData)
+    this.setState({todoData : newTodoData})
+  }
 
   render() {
     return(
@@ -41,11 +51,11 @@ export default class App extends Component {
           </div>
 
 
-        {this.todoData.map((data) => (
+        {this.state.todoData.map((data) => (
           <div style={this.getStyle()} key = {data.id}>
             <input type="checkbox" defaultChecked={false} />
               {data.title}
-            <button style={this.btnStyle}>x</button>
+            <button style={this.btnStyle} onClick = {() => this.handleClick(data.id)}>x</button>
           </div>
         ))}
         </div>
