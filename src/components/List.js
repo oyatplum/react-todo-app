bd25afae28fed2194b9758ca9d18949ac043b92c
yeji <oyatpeach@naver.com>
2023-01-29 14:54:10 +0900

    update css with tailwindcss

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,6 @@
 import React from 'react'
 
 export default function List({todoData, setTodoData}) {
-    const btnStyle = {
-        color: "#fff",
-        border: "none",
-        padding: "5px 9px",
-        borderRadius: "50%",
-        cursor: "pointer",
-        float: "right"
-      }
 
     const handleCompleteChange = (id) => {
         let newTodoData = todoData.map(data => {
@@ -18,14 +10,6 @@ export default function List({todoData, setTodoData}) {
         return data
         })
         setTodoData(newTodoData)
-    }
-
-    const getStyle = (completed) => {
-        return {
-        padding: "10px",
-        borderBottom: "1px #ccc dotted",
-        textDecoration: completed ? "line-through" : "none"
-        }
     }
 
     const handleClick = (id) => {
@@ -38,13 +22,18 @@ export default function List({todoData, setTodoData}) {
   return (
     <div>
         {todoData.map((data) => (
-          <div style={getStyle(data.completed)} key = {data.id}>
-            <input type="checkbox"
-            defaultChecked={false}
-            onChange = {() => handleCompleteChange(data.id)} />
-              {data.title}
-            <button style={btnStyle}
-            onClick = {() => handleClick(data.id)}>x</button>
+          <div key = {data.id}>
+            <div className='flex items-center justify-between w-full px-4 py-1 my-2 text-gray-600 bg-gray-100 border rounded'>
+              <div className='items-center'>
+                <input type="checkbox"
+                defaultChecked={false}
+                onChange = {() => handleCompleteChange(data.id)} />
+                <span className={data.completed ? "line-through": undefined}>{data.title}</span>
+              </div>
+              <div className='items-center'>
+                <button className = "px-4 py-2 float-right" onClick = {() => handleClick(data.id)}>x</button>
+              </div>
+            </div>
           </div>
         ))}
     </div>
